b784b59de3394f42d7d30648e6af7e3fde0258b0
Kapeet <itamar246@gmail.com>
2023-03-02 20:38:12 +0000

    fuck it i'm making it from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,38 @@
 use std::env;
-use std::io;
-
-extern crate sys_info;
-use sys_info::LinuxOSReleaseInfo;
+use std::{fs::File, io::Read, process};
 
 fn main() {
-	let os_info = sys_info::os_release().expect("Failed to get os info");
-	println!("Name: {}", os_info);
-	println!("Host: {}", sys_info::hostname().expect("Unknown hostname"));
-	let os_release_info = sys_info::linux_os_release().expect("Failed 2");
+	println!("Host: {}", get_host().expect("Failed"));
+	println!("CPU: {}", get_cpu_model().expect("Failed"));
 
-	match os_release_info.name {
-		Some(name) => println!("OS name: {}",name),
-		None => println!("OOPSIE!@!!@"),
+}
+
+
+pub fn get_host() -> Result<String, std::io::Error> {
+	let mut buf = String::new();
+	File::open("/sys/devices/virtual/dmi/id/product_name")?.read_to_string(&mut buf)?;
+	Ok(buf.trim().to_string())
+}
+
+
+pub fn get_cpu_model() -> Result<String, std::io::Error> {
+	let mut buffer = String::new();
+	let mut final_name = String::new();
+	File::open("/proc/cpuinfo")?.read_to_string(&mut buffer)?;
+	for line in buffer.lines() {
+		
+		if line.starts_with("model name") {
+			let splitted: Vec<&str> = line.split(":").collect();
+			
+			if let Some(name) = splitted.get(1) {
+				final_name = name.to_string().trim().to_string();
+				
+			}
+			else {
+				println!("failed");
+			}
+
+		}
 	}
-	match os_release_info.logo {
-		Some(logo) => println!("Logo: {}", logo),
-		None => println!("No logo."),
-	}
-//	let os_name = sys_info::LinuxOSReleaseInfo::name();
-//	println!("osname is {os_name:?}");
+	return Ok(final_name);
 }
