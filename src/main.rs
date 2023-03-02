593230f410d25cc815f483b5c4305c73361942b0
Kapeet <itamar246@gmail.com>
2023-03-02 21:02:39 +0000

    Add OS name with architecture

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
-use std::{fs::File, io::Read, process};
+use std::process::Command;
+use std::{fs::File, io::Read};
 
 fn main() {
+	println!("OS: {}", get_os_name().expect("Failed"));
 	println!("Host: {}", get_host().expect("Failed"));
 	println!("CPU: {}", get_cpu_model().expect("Failed"));
 
@@ -12,6 +13,38 @@ pub fn get_host() -> Result<String, std::io::Error> {
 	let mut buf = String::new();
 	File::open("/sys/devices/virtual/dmi/id/product_name")?.read_to_string(&mut buf)?;
 	Ok(buf.trim().to_string())
+}
+
+pub fn get_os_name() -> Result<String, std::io::Error> {
+	let mut buffer = String::new();
+	let mut name_without_quote_marks = String::new();
+	File::open("/etc/os-release")?.read_to_string(&mut buffer)?;
+	for line in buffer.lines() {
+		if line.starts_with("NAME=") {
+			let splitted: Vec<&str> = line.split("=").collect();
+			
+			if let Some(name) = splitted.get(1) {
+				// remove the quote marks
+				name_without_quote_marks = name.to_string().trim().to_string();
+				name_without_quote_marks.remove(0);
+				name_without_quote_marks.remove(name_without_quote_marks.len() - 1);
+
+				// add a space since we'll add the architecture in the same line
+				name_without_quote_marks.push(' ')
+			}
+		}
+	}
+
+	let architecture;
+	let output;
+	let mut input = Command::new("uname");
+	
+	input.arg("-m");
+	output = input.output().expect("Failed to read architecture info");
+	architecture = String::from_utf8(output.stdout).unwrap();
+	name_without_quote_marks.push_str(&architecture);
+
+	Ok(name_without_quote_marks)
 }
 
 
